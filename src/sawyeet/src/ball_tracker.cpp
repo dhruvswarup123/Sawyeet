1584dc5aac58681ac231d9de3b19f9639281d2db
Dhruv Swarup <dhruvswarup123@gmail.com>
2021-12-01 20:45:38 -0800

    converting to ros

--- src/sawyeet/src/ball_tracker.cpp
+++ src/sawyeet/src/ball_tracker.cpp
@@ -1,82 +1,237 @@
 /******************************************
- * OpenCV Tutorial: Ball Tracking using   *
- * Kalman Filter                          *
- * https://www.myzhar.com/blog/tutorials/tutorial-opencv-ball-tracker-using-kalman-filter/
+ * Kalman filter and tracker from https://www.myzhar.com/blog/tutorials/tutorial-opencv-ball-tracker-using-kalman-filter/
  ******************************************/
 
-// Module "core"
+#include "ros/ros.h"
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+
+#include "sensor_msgs/Image.h"
+#include "sensor_msgs/CameraInfo.h"
+#include "geometry_msgs/Point.h"
+
 #include <opencv2/core/core.hpp>
-
-// Module "highgui"
 #include <opencv2/highgui/highgui.hpp>
-
-// Module "imgproc"
 #include <opencv2/imgproc/imgproc.hpp>
-
-// Module "video"
 #include <opencv2/video/video.hpp>
 
-// Output
 #include <iostream>
-
-// Vector
 #include <vector>
 
+#include <cv_bridge/cv_bridge.h>
+
 using namespace std;
+using namespace sensor_msgs;
+using namespace geometry_msgs;
+using namespace message_filters;
 
-// >>>>> Color to be tracked
-#define MIN_H_BLUE 30
-#define MAX_H_BLUE 90
-// <<<<< Color to be tracked
+// >>>> Globals for kalman tracking
+int stateSize = 6;
+int measSize = 4;
+int contrSize = 0;
 
+unsigned int type = CV_32F;
 
-int main()
-{
-    // Camera frame
-    cv::Mat frame;
+double ticks = 0;
+bool found = false;
+int notFoundCount = 0;
+
+cv::KalmanFilter kf;
+cv::Mat state;
+cv::Mat meas;
+// <<<< Globals for kalman tracking
+
+void chatterCallback(const Image::ConstPtr& image_msg, const CameraInfo::ConstPtr& camerainfo_msg, const Points::ConstPtr& points_msg){
+    double precTick = ticks;
+    ticks = (double) (points_msg->header).stamp
+
+    // TODO: Get time difference here
+    double dT = ticks - precTick;
+
+    // >>>> Frame acquisition
+    cv_bridge::CvImagePtr cv_ptr;
+    cv::Mat frame, res;
+    
+    try{
+      cv_ptr = cv_bridge::toCvCopy(image_msg, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e){
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+    }
+
+    cv_ptr->image.copyTo(frame);
+    frame.copyTo(res);
+    // <<<< Frame acquisition
+
+    if (found)
+    {
+        // >>>> Matrix A
+        kf.transitionMatrix.at<float>(2) = dT;
+        kf.transitionMatrix.at<float>(9) = dT;
+        // <<<< Matrix A
+
+        state = kf.predict();
+
+        cv::Rect predRect;
+        predRect.width = state.at<float>(4);
+        predRect.height = state.at<float>(5);
+        predRect.x = state.at<float>(0) - predRect.width / 2;
+        predRect.y = state.at<float>(1) - predRect.height / 2;
+
+        cv::Point center;
+        center.x = state.at<float>(0);
+        center.y = state.at<float>(1);
+        cv::circle(res, center, 2, CV_RGB(255,0,0), -1);
+
+        cv::rectangle(res, predRect, CV_RGB(255,0,0), 2);
+    }
+
+    // >>>>> Noise smoothing
+    cv::Mat blur;
+    cv::GaussianBlur(frame, blur, cv::Size(5, 5), 3.0, 3.0);
+    // <<<<< Noise smoothing
+
+    // >>>>> HSV conversion
+    cv::Mat frmHsv;
+    cv::cvtColor(blur, frmHsv, cv::COLOR_BGR2HSV);
+    // <<<<< HSV conversion
+
+    // >>>>> Color Thresholding
+    // Note: change parameters for different colors
+    cv::Mat rangeRes = cv::Mat::zeros(frame.size(), CV_8UC1);
+
+    //TODO: Add if block for color?
+    cv::inRange(frmHsv, cv::Scalar(20, 80, 45),
+                        cv::Scalar(45, 255, 255), rangeRes);
+    // <<<<< Color Thresholding
+
+    // >>>>> Improving the result
+    cv::erode(rangeRes, rangeRes, cv::Mat(), cv::Point(-1, -1), 2);
+    // cv::dilate(rangeRes, rangeRes, cv::Mat(), cv::Point(-1, -1), 2);
+    // <<<<< Improving the result
+
+    // Thresholding viewing
+    cv::imshow("Threshold", rangeRes);
+
+    // >>>>> Contours detection
+    vector<vector<cv::Point> > contours;
+    cv::findContours(rangeRes, contours, cv::RETR_EXTERNAL,
+                        cv::CHAIN_APPROX_NONE);
+    // <<<<< Contours detection
+
+    // >>>>> Filtering
+    vector<vector<cv::Point> > balls;
+    vector<cv::Rect> ballsBox;
+    for (size_t i = 0; i < contours.size(); i++)
+    {
+        cv::Rect bBox;
+        bBox = cv::boundingRect(contours[i]);
+
+        float ratio = (float) bBox.width / (float) bBox.height;
+        if (ratio > 1.0f)
+            ratio = 1.0f / ratio;
+
+        // Searching for a bBox almost square
+        if (ratio > 0.75 && bBox.area() >= 400)
+        {
+            balls.push_back(contours[i]);
+            ballsBox.push_back(bBox);
+        }
+    }
+    // <<<<< Filtering
+
+    // >>>>> Detection result
+    for (size_t i = 0; i < balls.size(); i++)
+    {
+        cv::drawContours(res, balls, i, CV_RGB(20,150,20), -1);
+    }
+    // <<<<< Detection result
+
+    // >>>>> Kalman Update
+    if (balls.size() == 0)
+    {
+        notFoundCount++;
+        cout << "notFoundCount:" << notFoundCount << endl;
+        if( notFoundCount >= 100 )
+        {
+            found = false;
+        }
+    }
+    else
+    {
+        notFoundCount = 0;
+
+        meas.at<float>(0) = ballsBox[0].x + ballsBox[0].width / 2;
+        meas.at<float>(1) = ballsBox[0].y + ballsBox[0].height / 2;
+        meas.at<float>(2) = (float)ballsBox[0].width;
+        meas.at<float>(3) = (float)ballsBox[0].height;
+
+        if (!found) // First detection!
+        {
+            // >>>> Initialization
+            kf.errorCovPre.at<float>(0) = 1; // px
+            kf.errorCovPre.at<float>(7) = 1; // px
+            kf.errorCovPre.at<float>(14) = 1;
+            kf.errorCovPre.at<float>(21) = 1;
+            kf.errorCovPre.at<float>(28) = 1; // px
+            kf.errorCovPre.at<float>(35) = 1; // px
+
+            state.at<float>(0) = meas.at<float>(0);
+            state.at<float>(1) = meas.at<float>(1);
+            state.at<float>(2) = 0;
+            state.at<float>(3) = 0;
+            state.at<float>(4) = meas.at<float>(2);
+            state.at<float>(5) = meas.at<float>(3);
+            // <<<< Initialization
+
+            kf.statePost = state;
+            
+            found = true;
+        }
+        else
+            kf.correct(meas); // Kalman Correction
+    }
+    // <<<<< Kalman Update
+
+    // Final result
+    cv::imshow("Tracking", res);
+
+    // User key
+    ch = cv::waitKey(1);
+}
+
+int main(int argc, char **argv)
+{   
+    ros::init(argc, argv, "ball_tracker");
+    ros::NodeHandle nh;
+
+    ros::Subscriber sub = nh.subscribe("chatter", 1000, chatterCallback);
+
+    message_filters::Subscriber<Image> image_sub(nh, "image", 1);
+    message_filters::Subscriber<CameraInfo> info_sub(nh, "camera_info", 1);
+    message_filters::Subscriber<Point> points_sub(nh, "points", 1);
+
+    TimeSynchronizer<Image, CameraInfo, Point> sync(image_sub, info_sub, points_sub, 10);
+    sync.registerCallback(boost::bind(&callback, _1, _2, _3));
 
     // >>>> Kalman Filter
-    int stateSize = 6;
-    int measSize = 4;
-    int contrSize = 0;
+    
+    kf = cv::KalmanFilter(stateSize, measSize, contrSize, type);
 
-    unsigned int type = CV_32F;
-    cv::KalmanFilter kf(stateSize, measSize, contrSize, type);
-
-    cv::Mat state(stateSize, 1, type);  // [x,y,v_x,v_y,w,h]
-    cv::Mat meas(measSize, 1, type);    // [z_x,z_y,z_w,z_h]
-
-    // [E_x,E_y,E_v_x,E_v_y,E_w,E_h]
+    state = cv::Mat(stateSize, 1, type);  // [x,y,v_x,v_y,w,h]
+    meas = cv::Mat(measSize, 1, type);    // [z_x,z_y,z_w,z_h]
 
     // Transition State Matrix A
-    // Note: set dT at each processing step!
-    // [ 1 0 dT 0  0 0 ]
-    // [ 0 1 0  dT 0 0 ]
-    // [ 0 0 1  0  0 0 ]
-    // [ 0 0 0  1  0 0 ]
-    // [ 0 0 0  0  1 0 ]
-    // [ 0 0 0  0  0 1 ]
     cv::setIdentity(kf.transitionMatrix);
 
     // Measure Matrix H
-    // [ 1 0 0 0 0 0 ]
-    // [ 0 1 0 0 0 0 ]
-    // [ 0 0 0 0 1 0 ]
-    // [ 0 0 0 0 0 1 ]
     kf.measurementMatrix = cv::Mat::zeros(measSize, stateSize, type);
     kf.measurementMatrix.at<float>(0) = 1.0f;
     kf.measurementMatrix.at<float>(7) = 1.0f;
     kf.measurementMatrix.at<float>(16) = 1.0f;
     kf.measurementMatrix.at<float>(23) = 1.0f;
 
-    // Process Noise Covariance Matrix Q
-    // [ Ex   0   0     0     0    0  ]
-    // [ 0    Ey  0     0     0    0  ]
-    // [ 0    0   Ev_x  0     0    0  ]
-    // [ 0    0   0     Ev_y  0    0  ]
-    // [ 0    0   0     0     Ew   0  ]
-    // [ 0    0   0     0     0    Eh ]
-    //cv::setIdentity(kf.processNoiseCov, cv::Scalar(1e-2));
     kf.processNoiseCov.at<float>(0) = 1e-2;
     kf.processNoiseCov.at<float>(7) = 1e-2;
     kf.processNoiseCov.at<float>(14) = 5.0f;
@@ -88,185 +243,7 @@ int main()
     cv::setIdentity(kf.measurementNoiseCov, cv::Scalar(1e-1));
     // <<<< Kalman Filter
 
-    // Camera Index
-    int idx = 0;
+    ros::spin();
 
-    // Camera Capture
-    cv::VideoCapture cap;
-
-    // >>>>> Camera Settings
-    if (!cap.open("random/sample_720_tennis.mp4"))
-    {
-        cout << "Webcam not connected.\n" << "Please verify\n";
-        return EXIT_FAILURE;
-    }
-
-    cap.set(cv::CAP_PROP_FRAME_WIDTH, 1024);
-    cap.set(cv::CAP_PROP_FRAME_HEIGHT, 768);
-    // <<<<< Camera Settings
-
-    cout << "\nHit 'q' to exit...\n";
-
-    char ch = 0;
-
-    double ticks = 0;
-    bool found = false;
-
-    int notFoundCount = 0;
-
-    // >>>>> Main loop
-    while (ch != 'q' && ch != 'Q')
-    {
-        double precTick = ticks;
-        ticks = (double) cv::getTickCount();
-
-        double dT = (ticks - precTick) / cv::getTickFrequency(); //seconds
-
-        // Frame acquisition
-        cap >> frame;
-        if (frame.empty()) break;
-
-
-        cv::Mat res;
-        frame.copyTo( res );
-
-        if (found)
-        {
-            // >>>> Matrix A
-            kf.transitionMatrix.at<float>(2) = dT;
-            kf.transitionMatrix.at<float>(9) = dT;
-            // <<<< Matrix A
-
-            state = kf.predict();
-
-            cv::Rect predRect;
-            predRect.width = state.at<float>(4);
-            predRect.height = state.at<float>(5);
-            predRect.x = state.at<float>(0) - predRect.width / 2;
-            predRect.y = state.at<float>(1) - predRect.height / 2;
-
-            cv::Point center;
-            center.x = state.at<float>(0);
-            center.y = state.at<float>(1);
-            cv::circle(res, center, 2, CV_RGB(255,0,0), -1);
-
-            cv::rectangle(res, predRect, CV_RGB(255,0,0), 2);
-        }
-
-        // >>>>> Noise smoothing
-        cv::Mat blur;
-        cv::GaussianBlur(frame, blur, cv::Size(5, 5), 3.0, 3.0);
-        // <<<<< Noise smoothing
-
-        // >>>>> HSV conversion
-        cv::Mat frmHsv;
-        cv::cvtColor(blur, frmHsv, cv::COLOR_BGR2HSV);
-        // <<<<< HSV conversion
-
-        // >>>>> Color Thresholding
-        // Note: change parameters for different colors
-        cv::Mat rangeRes = cv::Mat::zeros(frame.size(), CV_8UC1);
-
-        //TODO: Add if block for color?
-        cv::inRange(frmHsv, cv::Scalar(20, 80, 45),
-                            cv::Scalar(45, 255, 255), rangeRes);
-        // <<<<< Color Thresholding
-
-        // >>>>> Improving the result
-        cv::erode(rangeRes, rangeRes, cv::Mat(), cv::Point(-1, -1), 2);
-        // cv::dilate(rangeRes, rangeRes, cv::Mat(), cv::Point(-1, -1), 2);
-        // <<<<< Improving the result
-
-        // Thresholding viewing
-        cv::imshow("Threshold", rangeRes);
-
-        // >>>>> Contours detection
-        vector<vector<cv::Point> > contours;
-        cv::findContours(rangeRes, contours, cv::RETR_EXTERNAL,
-                         cv::CHAIN_APPROX_NONE);
-        // <<<<< Contours detection
-
-        // >>>>> Filtering
-        vector<vector<cv::Point> > balls;
-        vector<cv::Rect> ballsBox;
-        for (size_t i = 0; i < contours.size(); i++)
-        {
-            cv::Rect bBox;
-            bBox = cv::boundingRect(contours[i]);
-
-            float ratio = (float) bBox.width / (float) bBox.height;
-            if (ratio > 1.0f)
-                ratio = 1.0f / ratio;
-
-            // Searching for a bBox almost square
-            if (ratio > 0.75 && bBox.area() >= 400)
-            {
-                balls.push_back(contours[i]);
-                ballsBox.push_back(bBox);
-            }
-        }
-        // <<<<< Filtering
-
-        // >>>>> Detection result
-        for (size_t i = 0; i < balls.size(); i++)
-        {
-            cv::drawContours(res, balls, i, CV_RGB(20,150,20), -1);
-        }
-        // <<<<< Detection result
-
-        // >>>>> Kalman Update
-        if (balls.size() == 0)
-        {
-            notFoundCount++;
-            cout << "notFoundCount:" << notFoundCount << endl;
-            if( notFoundCount >= 100 )
-            {
-                found = false;
-            }
-        }
-        else
-        {
-            notFoundCount = 0;
-
-            meas.at<float>(0) = ballsBox[0].x + ballsBox[0].width / 2;
-            meas.at<float>(1) = ballsBox[0].y + ballsBox[0].height / 2;
-            meas.at<float>(2) = (float)ballsBox[0].width;
-            meas.at<float>(3) = (float)ballsBox[0].height;
-
-            if (!found) // First detection!
-            {
-                // >>>> Initialization
-                kf.errorCovPre.at<float>(0) = 1; // px
-                kf.errorCovPre.at<float>(7) = 1; // px
-                kf.errorCovPre.at<float>(14) = 1;
-                kf.errorCovPre.at<float>(21) = 1;
-                kf.errorCovPre.at<float>(28) = 1; // px
-                kf.errorCovPre.at<float>(35) = 1; // px
-
-                state.at<float>(0) = meas.at<float>(0);
-                state.at<float>(1) = meas.at<float>(1);
-                state.at<float>(2) = 0;
-                state.at<float>(3) = 0;
-                state.at<float>(4) = meas.at<float>(2);
-                state.at<float>(5) = meas.at<float>(3);
-                // <<<< Initialization
-
-                kf.statePost = state;
-                
-                found = true;
-            }
-            else
-                kf.correct(meas); // Kalman Correction
-        }
-        // <<<<< Kalman Update
-
-        // Final result
-        cv::imshow("Tracking", res);
-
-        // User key
-        ch = cv::waitKey(1);
-    }
-    // <<<<< Main loop
-
-    return EXIT_SUCCESS;
+    return 0;
 }
---
+++ src/sawyeet/src/ball_tracker_working.cpp
@@ -0,0 +1,272 @@
+/******************************************
+ * Kalman filter and tracker from https://www.myzhar.com/blog/tutorials/tutorial-opencv-ball-tracker-using-kalman-filter/
+ ******************************************/
+
+#include "ros/ros.h"
+#include "geometry_msgs/Point.h"
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/video/video.hpp>
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void chatterCallback(const geometry_msgs::Point::ConstPtr& msg){
+
+}
+
+int main(int argc, char **argv)
+{
+
+  ros::init(argc, argv, "ball_tracker");
+  ros::NodeHandle n;
+  ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+  ros::spin();
+
+  return 0;
+}
+
+int main()
+{ 
+    // Camera frame
+    cv::Mat frame;
+
+    // >>>> Kalman Filter
+    int stateSize = 6;
+    int measSize = 4;
+    int contrSize = 0;
+
+    unsigned int type = CV_32F;
+    cv::KalmanFilter kf(stateSize, measSize, contrSize, type);
+
+    cv::Mat state(stateSize, 1, type);  // [x,y,v_x,v_y,w,h]
+    cv::Mat meas(measSize, 1, type);    // [z_x,z_y,z_w,z_h]
+
+    // [E_x,E_y,E_v_x,E_v_y,E_w,E_h]
+
+    // Transition State Matrix A
+    // Note: set dT at each processing step!
+    // [ 1 0 dT 0  0 0 ]
+    // [ 0 1 0  dT 0 0 ]
+    // [ 0 0 1  0  0 0 ]
+    // [ 0 0 0  1  0 0 ]
+    // [ 0 0 0  0  1 0 ]
+    // [ 0 0 0  0  0 1 ]
+    cv::setIdentity(kf.transitionMatrix);
+
+    // Measure Matrix H
+    // [ 1 0 0 0 0 0 ]
+    // [ 0 1 0 0 0 0 ]
+    // [ 0 0 0 0 1 0 ]
+    // [ 0 0 0 0 0 1 ]
+    kf.measurementMatrix = cv::Mat::zeros(measSize, stateSize, type);
+    kf.measurementMatrix.at<float>(0) = 1.0f;
+    kf.measurementMatrix.at<float>(7) = 1.0f;
+    kf.measurementMatrix.at<float>(16) = 1.0f;
+    kf.measurementMatrix.at<float>(23) = 1.0f;
+
+    // Process Noise Covariance Matrix Q
+    // [ Ex   0   0     0     0    0  ]
+    // [ 0    Ey  0     0     0    0  ]
+    // [ 0    0   Ev_x  0     0    0  ]
+    // [ 0    0   0     Ev_y  0    0  ]
+    // [ 0    0   0     0     Ew   0  ]
+    // [ 0    0   0     0     0    Eh ]
+    //cv::setIdentity(kf.processNoiseCov, cv::Scalar(1e-2));
+    kf.processNoiseCov.at<float>(0) = 1e-2;
+    kf.processNoiseCov.at<float>(7) = 1e-2;
+    kf.processNoiseCov.at<float>(14) = 5.0f;
+    kf.processNoiseCov.at<float>(21) = 5.0f;
+    kf.processNoiseCov.at<float>(28) = 1e-2;
+    kf.processNoiseCov.at<float>(35) = 1e-2;
+
+    // Measures Noise Covariance Matrix R
+    cv::setIdentity(kf.measurementNoiseCov, cv::Scalar(1e-1));
+    // <<<< Kalman Filter
+
+    // Camera Index
+    int idx = 0;
+
+    // Camera Capture
+    cv::VideoCapture cap;
+
+    // >>>>> Camera Settings
+    if (!cap.open("random/sample_720_tennis.mp4"))
+    {
+        cout << "Webcam not connected.\n" << "Please verify\n";
+        return EXIT_FAILURE;
+    }
+
+    cap.set(cv::CAP_PROP_FRAME_WIDTH, 1024);
+    cap.set(cv::CAP_PROP_FRAME_HEIGHT, 768);
+    // <<<<< Camera Settings
+
+    cout << "\nHit 'q' to exit...\n";
+
+    char ch = 0;
+
+    double ticks = 0;
+    bool found = false;
+
+    int notFoundCount = 0;
+
+    // >>>>> Main loop
+    while (ch != 'q' && ch != 'Q')
+    {
+        double precTick = ticks;
+        ticks = (double) cv::getTickCount();
+
+        double dT = (ticks - precTick) / cv::getTickFrequency(); //seconds
+
+        // Frame acquisition
+        cap >> frame;
+        if (frame.empty()) break;
+
+
+        cv::Mat res;
+        frame.copyTo( res );
+
+        if (found)
+        {
+            // >>>> Matrix A
+            kf.transitionMatrix.at<float>(2) = dT;
+            kf.transitionMatrix.at<float>(9) = dT;
+            // <<<< Matrix A
+
+            state = kf.predict();
+
+            cv::Rect predRect;
+            predRect.width = state.at<float>(4);
+            predRect.height = state.at<float>(5);
+            predRect.x = state.at<float>(0) - predRect.width / 2;
+            predRect.y = state.at<float>(1) - predRect.height / 2;
+
+            cv::Point center;
+            center.x = state.at<float>(0);
+            center.y = state.at<float>(1);
+            cv::circle(res, center, 2, CV_RGB(255,0,0), -1);
+
+            cv::rectangle(res, predRect, CV_RGB(255,0,0), 2);
+        }
+
+        // >>>>> Noise smoothing
+        cv::Mat blur;
+        cv::GaussianBlur(frame, blur, cv::Size(5, 5), 3.0, 3.0);
+        // <<<<< Noise smoothing
+
+        // >>>>> HSV conversion
+        cv::Mat frmHsv;
+        cv::cvtColor(blur, frmHsv, cv::COLOR_BGR2HSV);
+        // <<<<< HSV conversion
+
+        // >>>>> Color Thresholding
+        // Note: change parameters for different colors
+        cv::Mat rangeRes = cv::Mat::zeros(frame.size(), CV_8UC1);
+
+        //TODO: Add if block for color?
+        cv::inRange(frmHsv, cv::Scalar(20, 80, 45),
+                            cv::Scalar(45, 255, 255), rangeRes);
+        // <<<<< Color Thresholding
+
+        // >>>>> Improving the result
+        cv::erode(rangeRes, rangeRes, cv::Mat(), cv::Point(-1, -1), 2);
+        // cv::dilate(rangeRes, rangeRes, cv::Mat(), cv::Point(-1, -1), 2);
+        // <<<<< Improving the result
+
+        // Thresholding viewing
+        cv::imshow("Threshold", rangeRes);
+
+        // >>>>> Contours detection
+        vector<vector<cv::Point> > contours;
+        cv::findContours(rangeRes, contours, cv::RETR_EXTERNAL,
+                         cv::CHAIN_APPROX_NONE);
+        // <<<<< Contours detection
+
+        // >>>>> Filtering
+        vector<vector<cv::Point> > balls;
+        vector<cv::Rect> ballsBox;
+        for (size_t i = 0; i < contours.size(); i++)
+        {
+            cv::Rect bBox;
+            bBox = cv::boundingRect(contours[i]);
+
+            float ratio = (float) bBox.width / (float) bBox.height;
+            if (ratio > 1.0f)
+                ratio = 1.0f / ratio;
+
+            // Searching for a bBox almost square
+            if (ratio > 0.75 && bBox.area() >= 400)
+            {
+                balls.push_back(contours[i]);
+                ballsBox.push_back(bBox);
+            }
+        }
+        // <<<<< Filtering
+
+        // >>>>> Detection result
+        for (size_t i = 0; i < balls.size(); i++)
+        {
+            cv::drawContours(res, balls, i, CV_RGB(20,150,20), -1);
+        }
+        // <<<<< Detection result
+
+        // >>>>> Kalman Update
+        if (balls.size() == 0)
+        {
+            notFoundCount++;
+            cout << "notFoundCount:" << notFoundCount << endl;
+            if( notFoundCount >= 100 )
+            {
+                found = false;
+            }
+        }
+        else
+        {
+            notFoundCount = 0;
+
+            meas.at<float>(0) = ballsBox[0].x + ballsBox[0].width / 2;
+            meas.at<float>(1) = ballsBox[0].y + ballsBox[0].height / 2;
+            meas.at<float>(2) = (float)ballsBox[0].width;
+            meas.at<float>(3) = (float)ballsBox[0].height;
+
+            if (!found) // First detection!
+            {
+                // >>>> Initialization
+                kf.errorCovPre.at<float>(0) = 1; // px
+                kf.errorCovPre.at<float>(7) = 1; // px
+                kf.errorCovPre.at<float>(14) = 1;
+                kf.errorCovPre.at<float>(21) = 1;
+                kf.errorCovPre.at<float>(28) = 1; // px
+                kf.errorCovPre.at<float>(35) = 1; // px
+
+                state.at<float>(0) = meas.at<float>(0);
+                state.at<float>(1) = meas.at<float>(1);
+                state.at<float>(2) = 0;
+                state.at<float>(3) = 0;
+                state.at<float>(4) = meas.at<float>(2);
+                state.at<float>(5) = meas.at<float>(3);
+                // <<<< Initialization
+
+                kf.statePost = state;
+                
+                found = true;
+            }
+            else
+                kf.correct(meas); // Kalman Correction
+        }
+        // <<<<< Kalman Update
+
+        // Final result
+        cv::imshow("Tracking", res);
+
+        // User key
+        ch = cv::waitKey(1);
+    }
+    // <<<<< Main loop
+
+    return EXIT_SUCCESS;
+}
