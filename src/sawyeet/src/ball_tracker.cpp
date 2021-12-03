1edd7ae5364dbc648b77c9135e80558be12a6e0b
Dhruv Swarup <dhruvswarup123@gmail.com>
2021-12-02 16:51:20 -0800

    kalman filter for 3 coords

--- src/sawyeet/src/ball_tracker.cpp
+++ src/sawyeet/src/ball_tracker.cpp
@@ -1,26 +1,22 @@
 /******************************************
- * Kalman filter and tracker from https://www.myzhar.com/blog/tutorials/tutorial-opencv-ball-tracker-using-kalman-filter/
- ******************************************/
+Kalman filter and tracker from 
+https://www.myzhar.com/blog/tutorials/tutorial-opencv-ball-tracker-using-kalman-filter/
+*****************************************/
 
+#include "geometry_msgs/Point.h"
 #include "ros/ros.h"
+#include "sensor_msgs/CameraInfo.h"
+#include "sensor_msgs/Image.h"
+#include <Eigen/Dense>
+#include <cv_bridge/cv_bridge.h>
+#include <iostream>
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
-
-#include "sensor_msgs/Image.h"
-#include "sensor_msgs/CameraInfo.h"
-#include "geometry_msgs/Point.h"
-
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/video/video.hpp>
-
-#include <iostream>
 #include <vector>
-
-#include <cv_bridge/cv_bridge.h>
-
-#include <Eigen/Dense>
 
 #define ORANGE
 
@@ -39,8 +35,8 @@ using namespace geometry_msgs;
 using namespace message_filters;
 
 // >>>> Globals for kalman tracking
-int stateSize = 6;
-int measSize = 4;
+int stateSize = 8;
+int measSize = 5;
 int contrSize = 0;
 
 unsigned int type = CV_32F;
@@ -52,11 +48,14 @@ int notFoundCount = 0;
 cv::KalmanFilter kf;
 cv::Mat state;
 cv::Mat meas;
+float prev_measured_z = 0;
+
 // <<<< Globals for kalman tracking
 
+ros::Publisher chatter_pub;
+
 void chatterCallback(const Image::ConstPtr& image_msg, const CameraInfo::ConstPtr& camerainfo_msg, const Image::ConstPtr& depths_msg){
-    // >>>> First do the extraction of depths and cam matrix
-    //TODO easier way?
+    // >>>> Cam mat, depths
     Eigen::Matrix3f camera_matrix;
     camera_matrix(0, 0) = camerainfo_msg->K[0];
     camera_matrix(0, 1) = camerainfo_msg->K[1];
@@ -71,21 +70,18 @@ void chatterCallback(const Image::ConstPtr& image_msg, const CameraInfo::ConstPt
     cv::Mat depths;
     cv_bridge::CvImagePtr cv_depths_ptr;
     
-    try{ //TODO is the depth grayscale
-      cv_depths_ptr = cv_bridge::toCvCopy(depths_msg, sensor_msgs::image_encodings::TYPE_16UC1);
-      cv_depths_ptr->image.copyTo(depths);
+    try {
+        cv_depths_ptr = cv_bridge::toCvCopy(depths_msg, sensor_msgs::image_encodings::TYPE_8UC1);
+        cv_depths_ptr->image.copyTo(depths);
     }
     catch (cv_bridge::Exception& e){
-      ROS_ERROR("cv_bridge exception: %s", e.what());
-      return;
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
     }
-
-    // <<<< Depths
+    // <<<< Cam mat, depths
     
     double precTick = ticks;
     ticks = (depths_msg->header).stamp.toSec();
-
-    // TODO: Get time difference here
     double dT = ticks - precTick;
 
     // >>>> Frame acquisition
@@ -93,11 +89,11 @@ void chatterCallback(const Image::ConstPtr& image_msg, const CameraInfo::ConstPt
     cv::Mat frame, res;
     
     try{
-      cv_ptr = cv_bridge::toCvCopy(image_msg, sensor_msgs::image_encodings::BGR8);
+        cv_ptr = cv_bridge::toCvCopy(image_msg, sensor_msgs::image_encodings::BGR8);
     }
     catch (cv_bridge::Exception& e){
-      ROS_ERROR("cv_bridge exception: %s", e.what());
-      return;
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
     }
 
     cv_ptr->image.copyTo(frame);
@@ -107,28 +103,27 @@ void chatterCallback(const Image::ConstPtr& image_msg, const CameraInfo::ConstPt
     if (found)
     {
         // >>>> Matrix A
-        kf.transitionMatrix.at<float>(2) = dT;
-        kf.transitionMatrix.at<float>(9) = dT;
+        kf.transitionMatrix.at<float>(3) = dT;
+        kf.transitionMatrix.at<float>(12) = dT;
+        kf.transitionMatrix.at<float>(21) = dT;
         // <<<< Matrix A
 
         state = kf.predict();
-
+        
         cv::Rect predRect;
-        predRect.width = state.at<float>(4);
-        predRect.height = state.at<float>(5);
+        predRect.width = state.at<float>(6);
+        predRect.height = state.at<float>(7);
         predRect.x = state.at<float>(0) - predRect.width / 2;
         predRect.y = state.at<float>(1) - predRect.height / 2;
 
         cv::Point center;
         center.x = state.at<float>(0);
         center.y = state.at<float>(1);
-        cv::circle(res, center, 2, CV_RGB(255,0,0), -1);
+        float pred_depth = state.at<float>(2);
 
+        cv::circle(res, center, 2, CV_RGB(255,0,0), -1);
         cv::rectangle(res, predRect, CV_RGB(255,0,0), 2);
 
-        // >>>> Avg Depth
-        float avg_depth = depths.at<float>(center.x, center.y) * 0.001;
-        // <<<< Avg Depth
 
         // >>>> Homog pixels
         Eigen::Matrix<float, 3, 1> homog_pixels;
@@ -145,13 +140,23 @@ void chatterCallback(const Image::ConstPtr& image_msg, const CameraInfo::ConstPt
         // <<<< transformation from depths to realsense
 
         // >>> 3d coords
-        Eigen::Matrix<float, 3, 1> coords_3d = camera_matrix.inverse() * homog_pixels * avg_depth;
+        Eigen::Matrix<float, 3, 1> coords_3d = camera_matrix.inverse() * homog_pixels * pred_depth;
         coords_3d += camera_to_realsense;
+        cout << "Prev d: "<<  prev_measured_z << endl;
+        cout << "Pred d: "<<  meas.at<float>(2) << endl;
+
         // <<< 3d coords
 
         cout << "Center: " << center << endl;
         printf("3d coords: [%0.3f, %0.3f, %0.3f]\n", coords_3d[0], coords_3d[1], coords_3d[2]);
-        // cout << "3d coords: " << coords_3d << endl;
+
+        Point coords_3d_msg;
+
+        coords_3d_msg.x = coords_3d[0];
+        coords_3d_msg.y = coords_3d[1];
+        coords_3d_msg.z = coords_3d[2];
+        
+        chatter_pub.publish(coords_3d_msg);
     }
 
     // >>>>> Noise smoothing
@@ -222,7 +227,7 @@ void chatterCallback(const Image::ConstPtr& image_msg, const CameraInfo::ConstPt
     {
         notFoundCount++;
         cout << "notFoundCount:" << notFoundCount << endl;
-        if( notFoundCount >= 100 )
+        if( notFoundCount >= 15)
         {
             found = false;
         }
@@ -233,25 +238,50 @@ void chatterCallback(const Image::ConstPtr& image_msg, const CameraInfo::ConstPt
 
         meas.at<float>(0) = ballsBox[0].x + ballsBox[0].width / 2;
         meas.at<float>(1) = ballsBox[0].y + ballsBox[0].height / 2;
-        meas.at<float>(2) = (float)ballsBox[0].width;
-        meas.at<float>(3) = (float)ballsBox[0].height;
+
+        int temp_total_points = 0;
+        float temp_depth_sum = 0;
+
+        for (int i = 0; i < balls[0].size(); i++){
+            float temp_depth = depths.at<float>(balls[0][i].x, balls[0][i].y) * 0.001;
+            if ((temp_depth != 0) && (temp_depth <= 10.0)){
+                temp_total_points++;
+                temp_depth_sum += temp_depth;
+            }
+        }
+
+        float temp_depth_avg = temp_depth_sum/temp_total_points;
+        if (temp_total_points != 0 && temp_depth_avg > 0.0001){
+            meas.at<float>(2) = temp_depth_avg;
+            prev_measured_z = meas.at<float>(2);
+        }
+        else {
+            meas.at<float>(2) = 0;
+        }
+
+        meas.at<float>(3) = (float)ballsBox[0].width;
+        meas.at<float>(4) = (float)ballsBox[0].height;
 
         if (!found) // First detection!
         {
             // >>>> Initialization
-            kf.errorCovPre.at<float>(0) = 1; // px
-            kf.errorCovPre.at<float>(7) = 1; // px
-            kf.errorCovPre.at<float>(14) = 1;
-            kf.errorCovPre.at<float>(21) = 1;
-            kf.errorCovPre.at<float>(28) = 1; // px
-            kf.errorCovPre.at<float>(35) = 1; // px
+            kf.errorCovPre.at<float>(0) = 1;
+            kf.errorCovPre.at<float>(9) = 1;
+            kf.errorCovPre.at<float>(18) = 1;
+            kf.errorCovPre.at<float>(27) = 1;
+            kf.errorCovPre.at<float>(36) = 1;
+            kf.errorCovPre.at<float>(45) = 1;
+            kf.errorCovPre.at<float>(54) = 1;
+            kf.errorCovPre.at<float>(63) = 1;
 
             state.at<float>(0) = meas.at<float>(0);
             state.at<float>(1) = meas.at<float>(1);
-            state.at<float>(2) = 0;
+            state.at<float>(2) = meas.at<float>(2);
             state.at<float>(3) = 0;
-            state.at<float>(4) = meas.at<float>(2);
-            state.at<float>(5) = meas.at<float>(3);
+            state.at<float>(4) = 0;
+            state.at<float>(5) = 0;
+            state.at<float>(6) = meas.at<float>(3);
+            state.at<float>(7) = meas.at<float>(4);
             // <<<< Initialization
 
             kf.statePost = state;
@@ -281,6 +311,8 @@ int main(int argc, char **argv)
     // message_filters::Subscriber<Image> points_sub(nh, "/camera/aligned_depth_to_color/image_raw", 1);
     message_filters::Subscriber<Image> points_sub(nh, "/camera/depth/image_rect_raw", 1);
 
+    chatter_pub = nh.advertise<geometry_msgs::Point>("/sawyeet/ball_coords", 5);
+
     TimeSynchronizer<Image, CameraInfo, Image> sync(image_sub, info_sub, points_sub, 10);
     sync.registerCallback(boost::bind(&chatterCallback, _1, _2, _3));
 
@@ -295,18 +327,28 @@ int main(int argc, char **argv)
     cv::setIdentity(kf.transitionMatrix);
 
     // Measure Matrix H
+    //   x y z v v v w h
+    // [ 1 0 0 0 0 0 0 0] x
+    // [ 0 1 0 0 0 0 0 0] y
+    // [ 0 0 1 0 0 0 0 0] z
+    // [ 0 0 0 0 0 0 1 0] w
+    // [ 0 0 0 0 0 0 0 1] h
     kf.measurementMatrix = cv::Mat::zeros(measSize, stateSize, type);
     kf.measurementMatrix.at<float>(0) = 1.0f;
-    kf.measurementMatrix.at<float>(7) = 1.0f;
-    kf.measurementMatrix.at<float>(16) = 1.0f;
-    kf.measurementMatrix.at<float>(23) = 1.0f;
+    kf.measurementMatrix.at<float>(9) = 1.0f;
+    kf.measurementMatrix.at<float>(18) = 1.0f;
+    kf.measurementMatrix.at<float>(30) = 1.0f;
+    kf.measurementMatrix.at<float>(39) = 1.0f;
 
+    // cv::setIdentity(kf.processNoiseCov, cv::Scalar(1e-2));
     kf.processNoiseCov.at<float>(0) = 1e-2;
-    kf.processNoiseCov.at<float>(7) = 1e-2;
-    kf.processNoiseCov.at<float>(14) = 5.0f;
-    kf.processNoiseCov.at<float>(21) = 5.0f;
-    kf.processNoiseCov.at<float>(28) = 1e-2;
-    kf.processNoiseCov.at<float>(35) = 1e-2;
+    kf.processNoiseCov.at<float>(9) = 1e-2;
+    kf.processNoiseCov.at<float>(18) = 1e-2;
+    kf.processNoiseCov.at<float>(27) = 5.0f;
+    kf.processNoiseCov.at<float>(36) = 5.0f;
+    kf.processNoiseCov.at<float>(45) = 5.0f;
+    kf.processNoiseCov.at<float>(54) = 1e-2;
+    kf.processNoiseCov.at<float>(63) = 1e-2;
 
     // Measures Noise Covariance Matrix R
     cv::setIdentity(kf.measurementNoiseCov, cv::Scalar(1e-1));
