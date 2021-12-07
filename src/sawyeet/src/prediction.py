67cd473f7a7b2b57d55bacfc3e782d7f405b2037
Sumanth Kandala <sumanthkandala99@gmail.com>
2021-12-07 01:26:00 -0800

    vz

--- src/sawyeet/src/prediction.py
+++ src/sawyeet/src/prediction.py
@@ -18,9 +18,13 @@ import sys
 #CONSTANTS
 
 NUM_FRAMES = 5
-G = 9.81
-ALPHA = float(sys.argv[1])
-BETA = float(sys.argv[2])
+G = 9.8
+X_N = float(sys.argv[1])
+VX_N = float(sys.argv[2])
+VY_N = float(sys.argv[3])
+Y_N = float(sys.argv[4])
+VZ_N = float(sys.argv[5])
+DIST_Z = 60*2.54/100
 
 ###################################################################################
 #GLOBAL VARIABLES
@@ -32,6 +36,7 @@ curr_state = np.zeros((6,1))
 meas_state = np.zeros((6,1))
 pred_state = np.zeros((6,1))
 prev_time = None
+prev_z = 0
 
 xdata = []
 ydata = []
@@ -39,7 +44,7 @@ zdata = []
 
 curr_x = Value(c_double, 0)
 curr_y = Value(c_double, 0)
-curr_z = Value(c_double, 20)
+curr_z = Value(c_double, 20*100/2.54)
 published = Value('i', 0)
 color = Value('i', 0)
 last = Value('i', 0)
@@ -58,17 +63,17 @@ def runGraph(curr_x, curr_y, curr_z, published, color, last):
 
             if color.value == 0:
                 last.value = last.value + 1
-                ax.scatter3D(np.array(xdata), -np.array(zdata), np.array(ydata), c ='g')
+                ax.scatter3D(np.array(xdata)*100/2.54, -np.array(zdata)*100/2.54, np.array(ydata)*100/2.54, c ='g')
             else:
-                ax.scatter3D(np.array(xdata[:last.value]), -np.array(zdata[:last.value]), np.array(ydata[:last.value]), c = 'g')
-                ax.scatter3D(np.array(xdata[last.value:]), -np.array(zdata[last.value:]), np.array(ydata[last.value:]), c = 'r')
+                ax.scatter3D(np.array(xdata[:last.value])*100/2.54, -np.array(zdata[:last.value])*100/2.54, np.array(ydata[:last.value])*100/2.54, c = 'g')
+                ax.scatter3D(np.array(xdata[last.value:])*100/2.54, -np.array(zdata[last.value:])*100/2.54, np.array(ydata[last.value:])*100/2.54, c = 'r')
 
             ax.set_xlabel('x')
             ax.set_ylabel('z')
             ax.set_zlabel('y')
-            ax.set_xlim([-2, 2])
-            ax.set_ylim([-5, 0])
-            ax.set_zlim([-2, 2])
+            ax.set_xlim([-2*100/2.54, 2*100/2.54])
+            ax.set_ylim([-5*100/2.54, 2*100/2.54])
+            ax.set_zlim([-2*100/2.54, 2*100/2.54])
 
     ani = FuncAnimation(plt.gcf(), animate, interval=1, repeat=False)
     plt.show()
@@ -78,11 +83,11 @@ def runGraph(curr_x, curr_y, curr_z, published, color, last):
 #PREDICTED OUTPUT GRAPHING
 
 def plot_predicted():
-    global G, color, curr_x, curr_y, curr_z, curr_state
+    global G, color, curr_x, curr_y, curr_z, curr_state, DIST_Z
     color.value = 1
     delta = 0.01
     time = 0
-    while time < (-(curr_state[2])/curr_state[5]):
+    while time < (-(DIST_Z+curr_state[2])/curr_state[5]):
         curr_x.value = curr_state[0] + curr_state[3] * time
         curr_y.value = curr_state[1] + curr_state[4] * time - G * time**2 / 2. 
         curr_z.value = curr_state[2] + curr_state[5] * time
@@ -110,18 +115,19 @@ def intialize(coords):
 #STATE UPDATE AND KALMAN FILTER
 
 def stateCallback(coords):
-    global prev_state, prev_time, pred_state, curr_state, meas_state, IS_INITIALIZED, ALPHA, G, curr_x, curr_y, curr_z
+    global prev_state, prev_time, pred_state, curr_state, meas_state, IS_INITIALIZED, X_N, G, curr_x, curr_y, curr_z, Y_N, VX_N, VY_N, VZ_N
     curr_time = rospy.Time.now().to_sec() 
     dT = (curr_time - prev_time)
+
+    print("dt", dT)
     prev_time = curr_time
 
     if rospy.Time.now().to_sec() - prev_time > 0.3 or not IS_INITIALIZED:
         print("Initializing/Re-Initializing")
         intialize(coords)
         
+    #elif IS_INITIALIZED and abs(coords.x - meas_state[0]) < 0.5 and abs(coords.y - meas_state[1]) < 0.5 and abs(coords.z - meas_state[2]) < 0.5:
     else:
-    #IS_INITIALIZED and abs(coords.x - meas_state[0]) < 0.5 and abs(coords.y - meas_state[1]) < 0.5 and abs(coords.z - meas_state[2]) < 0.5:
-
         meas_state = np.zeros((6,1))
         meas_state[0] = coords.x
         meas_state[1] = coords.y
@@ -135,14 +141,17 @@ def stateCallback(coords):
         curr_z.value = meas_state[2]
 
         pred_state = np.zeros((6,1))
-        pred_state[0] = prev_state[0] + prev_state[3] * dT
-        pred_state[1] = prev_state[1] + prev_state[4] * dT - G * dT**2 / 2.
-        pred_state[2] = prev_state[2] + prev_state[5] * dT 
+        pred_state[0] = prev_state[0] + prev_state[3] * dT  
+        pred_state[1] = prev_state[1] + prev_state[4] * dT - G * dT**2 / 2. 
+        pred_state[2] = prev_state[2] + prev_state[5] * dT
         pred_state[3] = prev_state[3]
         pred_state[4] = prev_state[4] - G * dT
         pred_state[5] = prev_state[5]
 
-        update_matrix = np.array([[ALPHA],[ALPHA],[ALPHA],[BETA],[BETA],[BETA]])
+        print(coords.z)
+        #print(pred_state)
+
+        update_matrix = np.array([[X_N],[Y_N],[X_N],[VX_N],[VY_N],[VZ_N]])
         curr_state = pred_state + update_matrix * (meas_state - pred_state)
 
         prev_state = curr_state
@@ -152,8 +161,8 @@ def stateCallback(coords):
 
 def publish_pose(pred_state):
     des_pose = PoseStamped()
-    des_pose.pose.position.x = pred_state[0]
-    des_pose.pose.position.y = pred_state[1]
+    des_pose.pose.position.x = pred_state[0] + 0.15
+    des_pose.pose.position.y = pred_state[1] - 0.55
     des_pose.pose.position.z = pred_state[2]
     des_pose.pose.orientation.x = 0.
     des_pose.pose.orientation.y = 0.
@@ -165,15 +174,16 @@ def publish_pose(pred_state):
 #ESTIMATION FUNCTION -- ONCE BALL GOES OUT OF FRAME
 
 def estimation():
-    global prev_state, curr_state, meas_state, G
+    global prev_state, curr_state, meas_state, G, DIST_Z
     if rospy.Time.now().to_sec() - prev_time > 0.3 and curr_state[5] != 0:
-        pred_time = -(1+curr_state[2])/curr_state[5]
+        pred_time = -(DIST_Z+curr_state[2])/(curr_state[5])
         #print(pred_time)
+        #print(curr_state[3], curr_state[4], curr_state[5])
         pub_state = np.zeros(3)
         pub_state[0] = curr_state[0] + curr_state[3] * pred_time
         pub_state[1] = curr_state[1] + curr_state[4] * pred_time - G * pred_time**2 / 2. 
         meas = np.array([float(meas_state[0]), float(meas_state[1]), float(meas_state[2])])
-        print(np.round(meas,2), np.round(pub_state,2), np.round(curr_state[:3].reshape(-1),2))
+        #print(np.round(meas,2), np.round(pub_state,2)*100/2.54, np.round(curr_state[:3].reshape(-1),2))
         publish_pose(pub_state)
         return True
     return False
--- src/sawyeet/src/prediction2fordummies.py
+++ src/sawyeet/src/prediction2fordummies.py
@@ -15,13 +15,16 @@ import math
 import sys
 
 ###################################################################################
+#CONSTANTS
 
 NUM_FRAMES = 5
-G = 9.81
+G = 9.8
 ALPHA = float(sys.argv[1])
 BETA = float(sys.argv[2])
+DIST_Z = 1.52
 
 ###################################################################################
+#GLOBAL VARIABLES
 
 IS_INITIALIZED = False
 pub = rospy.Publisher('/sawyeet/des_end', PoseStamped, queue_size=1)
@@ -30,45 +33,20 @@ curr_state = np.zeros((6,1))
 meas_state = np.zeros((6,1))
 pred_state = np.zeros((6,1))
 prev_time = None
-curr_time = None
-old_time = None
-
-old_x = 0.
-old_y = 0.
-old_z = 0.
-curr_frame = 0
 
 xdata = []
 ydata = []
 zdata = []
 
-curr_x = Value(c_double, -20)
-curr_y = Value(c_double, -20)
-curr_z = Value(c_double, -20)
+curr_x = Value(c_double, 0)
+curr_y = Value(c_double, 0)
+curr_z = Value(c_double, 20)
 published = Value('i', 0)
 color = Value('i', 0)
 last = Value('i', 0)
 
 ###################################################################################
-
-def intialize(coords):
-    global prev_state, prev_time, IS_INITIALIZED, old_x, old_y, old_z, curr_state, pred_state, old_time
-    prev_time = rospy.Time.now().to_sec() 
-    prev_state[0] = coords.x
-    prev_state[1] = coords.y
-    prev_state[2] = coords.z
-    prev_state[3] = 0.
-    prev_state[4] = 0.
-    prev_state[5] = 0.
-    old_x = coords.x
-    old_y = coords.y
-    old_z = coords.z
-    curr_state = prev_state
-    pred_state = prev_state
-    IS_INITIALIZED = True
-    old_time = prev_time
-
-###################################################################################
+#GRAPHING & ANIMATION
 
 def runGraph(curr_x, curr_y, curr_z, published, color, last):
     def animate(i):
@@ -90,7 +68,7 @@ def runGraph(curr_x, curr_y, curr_z, published, color, last):
             ax.set_ylabel('z')
             ax.set_zlabel('y')
             ax.set_xlim([-2, 2])
-            ax.set_ylim([-5, 0])
+            ax.set_ylim([-5, 2])
             ax.set_zlim([-2, 2])
 
     ani = FuncAnimation(plt.gcf(), animate, interval=1, repeat=False)
@@ -98,9 +76,42 @@ def runGraph(curr_x, curr_y, curr_z, published, color, last):
     plt.close()
 
 ###################################################################################
+#PREDICTED OUTPUT GRAPHING
+
+def plot_predicted():
+    global G, color, curr_x, curr_y, curr_z, curr_state, DIST_Z
+    color.value = 1
+    delta = 0.01
+    time = 0
+    while time < (-(DIST_Z+curr_state[2])/curr_state[5]):
+        curr_x.value = curr_state[0] + curr_state[3] * time
+        curr_y.value = curr_state[1] + curr_state[4] * time - G * time**2 / 2. 
+        curr_z.value = curr_state[2] + curr_state[5] * time
+        time += delta 
+        sleep(0.1)
+
+###################################################################################
+#INITIALIZATIOon
+
+def intialize(coords):
+    global prev_state, prev_time, IS_INITIALIZED, curr_state, pred_state
+    prev_time = rospy.Time.now().to_sec() 
+    prev_state[0] = coords.x
+    prev_state[1] = coords.y
+    prev_state[2] = coords.z
+    prev_state[3] = 0.
+    prev_state[4] = 0.
+    prev_state[5] = 0.
+    curr_state = prev_state
+    pred_state = prev_state
+    IS_INITIALIZED = True
+    return
+
+###################################################################################
+#STATE UPDATE AND KALMAN FILTER
 
 def stateCallback(coords):
-    global prev_state, prev_time, pred_state, curr_state, meas_state, IS_INITIALIZED, ALPHA, G, curr_x, curr_y, curr_z, old_x, old_y, old_z, old_time, curr_frame, curr_time
+    global prev_state, prev_time, pred_state, curr_state, meas_state, IS_INITIALIZED, ALPHA, G, curr_x, curr_y, curr_z
     curr_time = rospy.Time.now().to_sec() 
     dT = (curr_time - prev_time)
     prev_time = curr_time
@@ -109,6 +120,7 @@ def stateCallback(coords):
         print("Initializing/Re-Initializing")
         intialize(coords)
         
+    #elif IS_INITIALIZED and abs(coords.x - meas_state[0]) < 0.5 and abs(coords.y - meas_state[1]) < 0.5 and abs(coords.z - meas_state[2]) < 0.5:
     else:
         meas_state = np.zeros((6,1))
         meas_state[0] = coords.x
@@ -118,15 +130,14 @@ def stateCallback(coords):
         meas_state[4] = (coords.y - prev_state[1])/dT
         meas_state[5] = (coords.z - prev_state[2])/dT
 
-        if curr_frame > 0:
-            curr_x.value = meas_state[0]
-            curr_y.value = meas_state[1]
-            curr_z.value = meas_state[2]
+        curr_x.value = meas_state[0]
+        curr_y.value = meas_state[1]
+        curr_z.value = meas_state[2]
 
         pred_state = np.zeros((6,1))
-        pred_state[0] = prev_state[0] + prev_state[3] * dT
-        pred_state[1] = prev_state[1] + prev_state[4] * dT - G * dT**2 / 2.
-        pred_state[2] = prev_state[2] + prev_state[5] * dT 
+        pred_state[0] = prev_state[0] + prev_state[3] * dT  
+        pred_state[1] = prev_state[1] + prev_state[4] * dT - G * dT**2 / 2. 
+        pred_state[2] = prev_state[2] + prev_state[5] * dT# - ETA*prev_state[5]
         pred_state[3] = prev_state[3]
         pred_state[4] = prev_state[4] - G * dT
         pred_state[5] = prev_state[5]
@@ -134,26 +145,16 @@ def stateCallback(coords):
         update_matrix = np.array([[ALPHA],[ALPHA],[ALPHA],[BETA],[BETA],[BETA]])
         curr_state = pred_state + update_matrix * (meas_state - pred_state)
 
-        curr_frame = curr_frame + 1
-
-        if curr_frame % NUM_FRAMES == 0:
-            #ballistic_estimate()
-            old_x = curr_state[0]
-            old_y = curr_state[1]
-            old_z = curr_state[2]
-            curr_frame = 0
-            old_time = curr_time
-
-
         prev_state = curr_state
 
 ###################################################################################
+#PUBLISH FUNCTION
 
-def publish_pose(curr_state):
+def publish_pose(pred_state):
     des_pose = PoseStamped()
-    des_pose.pose.position.x = curr_state[0]
-    des_pose.pose.position.y = curr_state[1]
-    des_pose.pose.position.z = curr_state[2]
+    des_pose.pose.position.x = pred_state[0]
+    des_pose.pose.position.y = pred_state[1]+.13
+    des_pose.pose.position.z = pred_state[2]
     des_pose.pose.orientation.x = 0.
     des_pose.pose.orientation.y = 0.
     des_pose.pose.orientation.z = 0.
@@ -161,75 +162,24 @@ def publish_pose(curr_state):
     pub.publish(des_pose)
      
 ###################################################################################
-#TODO
+#ESTIMATION FUNCTION -- ONCE BALL GOES OUT OF FRAME
+
 def estimation():
-    global prev_state, curr_state, meas_state, G, prev_time
+    global prev_state, curr_state, meas_state, G, DIST_Z
     if rospy.Time.now().to_sec() - prev_time > 0.3 and curr_state[5] != 0:
-        pred_time = -(curr_state[2])/curr_state[5]
+        pred_time = -(DIST_Z+curr_state[2])/(curr_state[5])
         #print(pred_time)
         pub_state = np.zeros(3)
         pub_state[0] = curr_state[0] + curr_state[3] * pred_time
         pub_state[1] = curr_state[1] + curr_state[4] * pred_time - G * pred_time**2 / 2. 
         meas = np.array([float(meas_state[0]), float(meas_state[1]), float(meas_state[2])])
-        print(np.round(meas,2), np.round(pub_state,2), np.round(curr_state[:3].reshape(-1),2))
-        publish_pose(meas_state)
-        return True
-    return False
-
-####################################################################################
-
-def ballistic_estimation():
-    global prev_state, curr_state, meas_state, G, prev_time, curr_time
-    if rospy.Time.now().to_sec() - prev_time > 0.3 and curr_state[5] != 0:
-        dt = curr_time - old_time
-        print(dt)
-        vx = (curr_state[0] - old_x)/dt
-        vz = (curr_state[1] - old_z)/dt
-        if vz != 0:
-            t = -curr_state[2]/vz
-            vy = (curr_state[1] - old_y + 9.81*t**2/2.)/t
-            index = 0
-            print("BALLISTIC")
-            print(vx, vy, vz)
+        print(np.round(meas,2), np.round(pub_state,2)*100/2.54, np.round(curr_state[:3].reshape(-1),2))
+        publish_pose(pub_state)
         return True
     return False
 
 ###################################################################################
-
-def plot_predicted():
-    global G, color, curr_x, curr_state
-    color.value = 1
-    delta = 0.01
-    time = 0
-    dt = curr_time - old_time
-    print(dt)
-    vx = (curr_state[0] - old_x)/dt
-    vz = (curr_state[1] - old_z)/dt
-    if vz != 0:
-        t = -curr_state[2]/vz
-        vy = (curr_state[1] - old_y + 9.81*t**2/2.)/t
-        while time < (-curr_state[2])/vz:
-            curr_x.value = old_x + vx * time
-            curr_y.value = old_y + vy * time - G * time**2 / 2. 
-            curr_z.value = old_z + vz * time
-            time += delta 
-            sleep(0.1)
-
-###################################################################################
-
-def ballistic_plot_predicted():
-    global G, color, curr_x
-    color.value = 1
-    delta = 0.01
-    time = 0
-    while time < (-(curr_state[2])/curr_state[5]):
-        curr_x.value = curr_state[0] + curr_state[3] * time
-        curr_y.value = curr_state[1] + curr_state[4] * time - G * time**2 / 2. 
-        curr_z.value = curr_state[2] + curr_state[5] * time
-        time += delta 
-        sleep(0.1)
-
-###################################################################################
+#MAIN LISTENER
 
 def listener():
     global prev_time, curr_state, curr_x, curr_y, curr_z, published, color
@@ -253,3 +203,5 @@ def listener():
 
 if __name__ == '__main__':
     listener()
+
+###################################################################################
